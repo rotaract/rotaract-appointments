6e1e1ac13ee70df54630e7199b85e843cd780848
Benno Bielmeier <github@bbenno.com>
2021-07-17 15:20:55 +0200

    Add file header to settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,15 @@
-new lc_select('select.lc_select', {
-	enable_search: true,
-	labels: lcData.labels
-});
+/**
+ * Custom JS intended to be included in the plugin's setting page.
+ *
+ * @author zimbcode
+ * @package Rotaract-Appointments
+ */
+
+/** Initialize LC-select field. */
+new lc_select(
+	'select.lc_select',
+	{
+		enable_search: true,
+		labels: lcData.labels
+	}
+);

4321a5070dd08242c36cd621ba68e87a797afe66
Benno Bielmeier <github@bbenno.com>
2021-07-20 15:02:04 +0200

    PHP: Translate owner type

--- admin/partials/field-appointment-owners.php
+++ admin/partials/field-appointment-owners.php
@@ -20,7 +20,7 @@
 		<option value="Rotaract Deutschland Komitee"<?php echo in_array( 'Rotaract Deutschland Komitee', $options[ $args['label_for'] ], true ) ? ' selected' : ''; ?>>Rotaract Deutschland Komitee</option>
 	</optgroup>
 	<?php foreach ( $owners as $name => $items ) : ?>
-	<optgroup label="<?php esc_attr_e( $name, 'rotaract-appointments' ); ?>">
+	<optgroup label="<?php echo esc_attr( localize_owner_type_name( $name ) ); ?>">
 		<?php foreach ( $items as $item ) : ?>
 		<option value="<?php echo esc_attr( $item ); ?>"<?php echo in_array( $item, $options[ $args['label_for'] ], true ) ? ' selected' : ''; ?>><?php echo esc_html( $item ); ?></option>
 		<?php endforeach; ?>

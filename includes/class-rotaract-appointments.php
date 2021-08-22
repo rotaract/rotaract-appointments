rotaract/rotaract-appointments

commit 0ae68c1dabfe782bd04aa73022e70fb70de24319

Revert rename mistake

includes/class-rotaract-appointments.php
@@ -45,7 +45,7 @@ class Rotaract_Appointments {
 	 * @access   protected
 	 * @var      string    $rotaract_appointments    The string used to uniquely identify this plugin.
 	 */
-	protected string $rotaract_appointments;
+	protected string $plugin_name;
 
 	/**
 	 * The current version of the plugin.
@@ -164,7 +164,7 @@ class Rotaract_Appointments {
 	 */
 	private function define_admin_hooks() {
 
-		$plugin_admin = new Rotaract_Appointments_Admin( $this->get_rotaract_appointments(), $this->get_version(), $this->elastic_caller );
+		$plugin_admin = new Rotaract_Appointments_Admin( $this->get_plugin_name(), $this->get_version(), $this->elastic_caller );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
@@ -185,7 +185,7 @@ class Rotaract_Appointments {
 	 */
 	private function define_public_hooks() {
 
-		$plugin_public = new Rotaract_Appointments_Public( $this->get_rotaract_appointments(), $this->get_version(), $this->elastic_caller );
+		$plugin_public = new Rotaract_Appointments_Public( $this->get_plugin_name(), $this->get_version(), $this->elastic_caller );
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
@@ -209,8 +209,8 @@ class Rotaract_Appointments {
 	 * @since     1.0.0
 	 * @return    string    The name of the plugin.
 	 */
-	public function get_rotaract_appointments(): string {
-		return $this->rotaract_appointments;
+	public function get_plugin_name(): string {
+		return $this->plugin_name;
 	}
 
 	/**

commit d5d2a5cae587e5231e2107d24485d53ee2bb7b21

Fix bug introduced in 0ae68c1d

includes/class-rotaract-appointments.php
@@ -43,7 +43,7 @@ class Rotaract_Appointments {
 	 *
 	 * @since    1.0.0
 	 * @access   protected
-	 * @var      string    $rotaract_appointments    The string used to uniquely identify this plugin.
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
 	 */
 	protected string $plugin_name;
 
@@ -80,7 +80,7 @@ class Rotaract_Appointments {
 		} else {
 			$this->version = '1.0.0';
 		}
-		$this->rotaract_appointments = 'rotaract-appointments';
+		$this->plugin_name = 'rotaract-appointments';
 
 		$this->load_dependencies();
 		$this->set_locale();